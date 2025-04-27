0712a83ac2ec7d28498951775555707ddb71d291
gruzam0615 <gruzam0615@gmail.com>
2025-04-27 16:26:52 +0900

    main.py     generate random number

---
+++ main.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta, timezone
+import random
+
+'''
+now - nDay == 어제
+현재시간서 nDay timedelta(days=n) 만큼 감소
+now() - timedelta(days=1)은 현재시간보다 하루이전 값을 출력
+'''
+now = datetime.now()
+# print("now:", now)
+standardDay = now + timedelta(-5)
+# print("standardDay: ", standardDay)
+KST = timezone(timedelta(hours=9))
+# time1 = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond, tzinfo=KST)
+# print("time1:", time1)
+# time2 = datetime.datetime(now.year, now.month, now.day, 18, 0, now.second, now.microsecond, tzinfo=KST)
+# print("time2:", time2)
+time3 = datetime(standardDay.year, standardDay.month, standardDay.day, standardDay.hour, standardDay.minute, standardDay.second, standardDay.microsecond, tzinfo=KST)
+# time3 = datetime(standardDay.year, standardDay.month, standardDay.day, 20, 45, standardDay.second, standardDay.microsecond, tzinfo=KST)
+# print("time3:", time3)
+# formatedTime = now.strftime("%Y%m%d%H%M%S%M") #year-month-day-hour-minutes-sec-msec
+
+numberlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+             11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+             21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+             31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+             41, 42, 43, 44, 45
+            ]
+
+# result = random.sample(numberlist, 1) # [n]
+result = []
+count = 0
+
+while len(result) < 6:
+    seedTime = time3 + timedelta(count)
+    print("seedTime:", seedTime)
+
+    seedValue = seedTime.strftime("%Y%m%d%H%M%S%m")
+    # print(seedValue)
+
+    random.seed(int(seedValue))
+
+    temp = random.randint(0, len(numberlist))
+    value = numberlist[temp]
+    print("temp:", temp)
+    # print("value:", value)
+    result.append(value)
+    numberlist.remove(value)
+    count = count + 1
+
+
+result.sort()
+# print("seedValue:", seedValue)
+print("result:", result)
